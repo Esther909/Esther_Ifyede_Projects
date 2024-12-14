31048c4058ed6c2d32931590c8c0c0df5f8d004c
Esther Ifyede <70719062+Esther909@users.noreply.github.com>
2024-12-14 11:15:26 +0100

    Update and rename case study 1.sql to Evaluating_Employee_Trend/My_Approach

--- case study 1.sql
+++
@@ -1,9 +0,0 @@
--- 1. What is the total amount each customer spent at the restaurant?
-select s.customer_id,
-sum(m.price) total_spent
-from
-dannys_diner.sales s
-left join dannys_diner.menu m using (product_id)
-group by 1
-order by 1;
--- Customer A is the highest spending customer with 76
